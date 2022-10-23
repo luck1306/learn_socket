bcd92fda1aeb0b7fd8d79ba4e65788d733aad578
luck1306 <luck1306@naver.com>
2022-10-23 21:03:30 +0900

    connect socket server-client

--- src/main/java/com/example/learn_socket/hadler/WebSocketHandler.java
+++ src/main/java/com/example/learn_socket/hadler/WebSocketHandler.java
@@ -1,2 +1,20 @@
-package com.example.learn_socket.hadler;public class WebSocketHandler {
+package com.example.learn_socket.hadler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Slf4j
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception{
+        String payload = message.getPayload();
+        log.info(String.format("payload : { %s }", payload));
+        TextMessage initialGreeting = new TextMessage("Welcome this chat server!");
+        session.sendMessage(initialGreeting);
+    }
 }
